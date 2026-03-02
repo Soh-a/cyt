b1897581e5ea1667b1597a02dbc4160f004bb099
Soh-a <gouravsarmah2012@gmail.com>
2026-03-02 17:44:14 +0530

    Update ai.js

--- api/ai.js
+++ api/ai.js
@@ -16,7 +16,7 @@ router.post("/", async (req, res) => {
 
     // Call Gemini 1.5 Flash model
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
