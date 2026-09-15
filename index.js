12aa886ce4423c26ff5f56489909ae9660f73175
Soh-a <gouravsarmah2012@gmail.com>
2026-09-15 20:11:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,10 +33,12 @@ app.use(express.json());
 // ----------------------
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert(
-            JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
-        ),
-        databaseURL: process.env.FIREBASE_DATABASE_URL || process.env.FIREBASE_DB_URL
+        credential: admin.credential.cert({
+            projectId: process.env.FIREBASE_PROJECT_ID,
+            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+        }),
+        databaseURL: process.env.FIREBASE_DATABASE_URL
     });
 }
 
